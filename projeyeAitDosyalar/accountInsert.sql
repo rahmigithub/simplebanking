483b2236c8d7bbf00de4ea4c756035d592d90f10
rahmigithub <alkanrahmi93@gmail.com>
2025-01-05 20:02:20 +0300

    added Useful files for the project

---
+++ projeyeAitDosyalar/accountInsert.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.account (id,account_number,balance,"owner",create_date) VALUES
+	 ('501b6b11-e7d6-414d-a35e-18e2d06eee8b'::uuid,'669-7788',0.0,'Kerem Karaca','2025-01-04 23:16:51.448');
